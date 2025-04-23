a483e24a66cfa6ae3af6af35f3b537600b1f02a0
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2025-04-23 05:09:04 -0500

    update game_world and implemented basic driver

--- Source/Maps/game_world.py
+++ Source/Maps/game_world.py
@@ -1,14 +1,11 @@
 import pygame
-from map_generator import map_generator
+from Source.Maps.map_generator import map_generator
 
 WIDTH, HEIGHT = 900, 600  # Pygame Window Width and Height
 FPS = 60  # Sets the Frames Per Second for game
 Py_Window = pygame.display.set_mode((WIDTH, HEIGHT))  # Create py window
 pygame.display.set_caption("Game Platformer")  # Window Game Caption
 
-def draw_window():  # Function for background arguments
-	world.draw()
-	pygame.display.update()  # Update game loop
 
 class World:  # Create new class world
 	def __init__(self, data):
@@ -38,26 +35,3 @@ class World:  # Create new class world
 	def draw(self):
 		for tile in self.tile_list:
 			Py_Window.blit(tile[0], tile[1])
-
-test = map_generator()
-world = World(test.generate_map_array())
-
-test.print_array()
-
-# Def main contains main game loop components only
-def main():
-	clock = pygame.time.Clock()  # Control time of main function
-	run = True
-	while run:
-		clock.tick(FPS)  # Controls the speed of the while loop
-		for event in pygame.event.get():  # For loop looks for events
-			if event.type == pygame.QUIT:  # User quit window
-				run = False
-
-		draw_window()  # Call function
-
-	pygame.quit()  # quits the game loop and exits window
-
-
-if __name__ == "__main__":  # Ensures code only runs file in file
-	main()
--- Source/driver.py
+++ Source/driver.py
@@ -1,24 +1,32 @@
 # Import Files/Modules
-from Source.Maps.game_world import game_world
+from Source.Maps.game_world import World
 from Source.Maps.map_generator import map_generator
 import pygame
 pygame.init()
 
+FPS = 60  # Sets the Frames Per Second for game
+
+test = map_generator()
+world = World(test.generate_map_array())
+
+def draw_window():  # Function for background arguments
+	world.draw()
+	pygame.display.update()  # Update game loop
+
+# Def main contains main game loop components only
 def main():
-    test = map_generator()
+	clock = pygame.time.Clock()  # Control time of main function
+	run = True
+	while run:
+		clock.tick(FPS)  # Controls the speed of the while loop
+		for event in pygame.event.get():  # For loop looks for events
+			if event.type == pygame.QUIT:  # User quit window
+				run = False
 
-    world = game_world()
+		draw_window()  # Call function
 
-    run = True
-    while run:
-        for event in pygame.event.get():  # For loop looks for events
-            if event.type == pygame.QUIT:  # User quit window
-                run = False
+	pygame.quit()  # quits the game loop and exits window
 
-        world.draw_window()
 
-    pygame.quit()  # quits the game loop and exits window
-
-if __name__ == '__main__':
-    main()
-
+if __name__ == "__main__":  # Ensures code only runs file in file
+	main()
