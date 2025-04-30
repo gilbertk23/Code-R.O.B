c900857751eedcaf80e7a4df393e79260dec7b20
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2025-04-30 09:10:00 -0500

    fixed problems

--- Source/Game_Windows/game_loop.py
+++ Source/Game_Windows/game_loop.py
@@ -1,12 +1,8 @@
 # Import Files/Modules
-from turtledemo.penrose import start
-
-from Source.Game_Windows.main_menu import main_menu
 from Source.Maps.game_world import world
 from Source.Maps.map_generator import map_generator
 from Source.Interactors.main_character import player
 from Source.Game_Windows.default_window import default_window
-from Source.Assets.preset_maps import start_map
 import pygame
 
 pygame.init()
@@ -30,27 +26,19 @@ class game_loop:
 
     # Helpers
     def generate_new_map(self):
-        new_map = self.game_world.reset_map()
-        self.main_character.portal_active = False
-        return new_map
+        if self.main_character.portal_active:
+            self.game_world.reset_map()
+            self.main_character.portal_active = False
+            pygame.display.update()
 
     def draw_window(self, main_character: player) -> None:
         pygame.Surface.fill(self.window, (255, 255, 0))
         pygame.draw.rect(self.window, (255, 0, 0), (100, 100, default_window().get_window_width() - 200, default_window().get_window_height() - 200))
         self.window.blit(self.text, (default_window().get_window_width() / 2.7, default_window().get_window_height() / 20))
-        self.generate_new_map().draw(self.window)
+        self.game_world.draw(self.window)
         main_character.update_player(self.game_world.get_tile_list())
+        self.generate_new_map()
         pygame.display.update()
-
-    def map_levels(self):
-        game_world = []
-        if map_generator.get_map_count == 1:
-            game_world = start_map()
-
-        elif self.main_character.portal_active:
-            game_world = self.generate_new_map()
-        return game_world
-
 
     def run_game(self):
         pygame.init()
