fe60990c9f468c859cfa36f3d8e1b6185623567a
Jerry Cai <jerrycai92@gmail.com>
2025-05-09 01:06:53 -0500

    Update driver.py

--- Source/driver.py
+++ Source/driver.py
@@ -3,7 +3,16 @@ from Source.Game_Windows.game_loop import game_loop
 
 # Def main contains main game loop components only
 def main() -> None:
-    game_loop().run_game()
+    try:
+        game_loop().run_game()
+    except TimeoutError:
+        print("Game has timed out and failed to load.")
+    except ImportError:
+        print("Source or pygame has failed to import, please make sure to install all of the packages.")
+    except KeyboardInterrupt:
+        print("You have exited the program yourself, game has successfully closed.")
+    except Exception as e:
+        print("Error: ", e)
 
 if __name__ == "__main__":  # Ensures code only runs file in file
     main()
